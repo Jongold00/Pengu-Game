7cd46b3f6e958fb34e76853517b06224f4900a41
JoberXeven <45186143+JoberXeven@users.noreply.github.com>
2020-02-11 20:11:21 -0500

    WaterMovement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     public float jumpPow = 10f;
     public float swimSpeed = 7;
     public float maxSwimSpeed = 5f;
+    public float maxVel = 8;
     public Rigidbody2D rgdb;
     bool underWater = false;
     bool canJump = true;
@@ -25,29 +26,25 @@ public class PlayerController : MonoBehaviour
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        float maxVel = 14;
-        rgdb.velocity = Vector2.ClampMagnitude(rgdb.velocity, maxVel);
-        Vector2 waterVelocity = new Vector2(x, y) * swimSpeed * Time.deltaTime;
+        
         if (underWater) {
-            //Vector2 waterVelocity = new Vector2(x, y) * swimSpeed * Time.deltaTime;
-            
-
+            Vector2 waterVelocity = new Vector2(x, y) * swimSpeed * Time.deltaTime;
             //rgdb.velocity = (waterVelocity);
             rgdb.AddForce(waterVelocity);
+            rgdb.velocity = new Vector2(rgdb.velocity.x, rgdb.velocity.y - .02f);
             Vector2.ClampMagnitude(rgdb.velocity, maxSwimSpeed);
 
         }
         else {
-            sliding = Input.GetKey(KeyCode.S);
-            Debug.Log(x);
-
             rgdb.velocity = Vector2.ClampMagnitude(rgdb.velocity, maxVel);
+            sliding = Input.GetKey(KeyCode.S);
             if (sliding) {
                 rgdb.AddForce(Vector2.right * x * slideSpeed);
                 
             }
             else {
-                rgdb.velocity = new Vector2(1 * x * walkSpeed * Time.deltaTime, rgdb.velocity.y);
+                //transform.Translate(new Vector2(x * walkSpeed/10 * Time.deltaTime, y));
+                rgdb.velocity = new Vector2(x * walkSpeed * Time.deltaTime, rgdb.velocity.y);
             }
             
         }
@@ -59,15 +56,15 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Ocean")) {
-            rgdb.velocity = new Vector2(rgdb.velocity.x * .5f, rgdb.velocity.x * .5f);
-            rgdb.gravityScale = .1f;
+            rgdb.velocity = new Vector2(rgdb.velocity.x, rgdb.velocity.x * .5f);
+            rgdb.gravityScale = 0f;
             underWater = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
         if (collision.CompareTag("Ocean")) {
-            Invoke("delayGrav", 0f);
+            rgdb.gravityScale = 1f;
             underWater = false;
         }
     }
